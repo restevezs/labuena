a091ee2729561e90e5cd8bc5b77e6313503f34a9
restevezs <restevezs>
2021-09-21 13:18:14 -0600

    added a new

--- diff_labels.py
+++ diff_labels.py
@@ -56,6 +56,7 @@ def variants_collected(Variant_to_compile):
 def edited_lines(idcommit):
 
     label_fromsjson = []
+    EmptyValue = []
     Openjson = open("labels.json", "r")
     data_json = json.loads(Openjson.read())
     for i in data_json['labels']:
